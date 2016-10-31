9b2fc869cf678110162a7f945ea851567483a158
tanner.cherry <tanner.cherry@gmail.com>
2016-10-31 13:48:49 -0500

    "in-class test"

--- pollsApp/wsgi.py
+++ pollsApp/wsgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-import django.core.handlers.wsgi
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollsApp.settings")
+
+import django.core.handlers.wsgi
 
 application = django.core.handlers.wsgi.WSGIHandler()
 
-application = get_wsgi_application()
+
+"""from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()"""
